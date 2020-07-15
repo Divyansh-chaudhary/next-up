83d9b87e20bab0a2409cb9c86baa30e6a71699c0
Divyansh-chaudhary <divyanshch2@gmail.com>
2020-07-14 19:03:11 -0800

    header almost completed zoom effect left

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(document).ready( function(){
+
+    /******** document ready animation ****/
+    setTimeout( function() {
+        $(".head-animation").hide();
+    }, 1300);
+
+    /*******   menu show and hide  ********/
     $("nav .btn").click( function(){
         $(this).toggleClass("cross");
         $("nav ul").toggleClass("ul-show");
