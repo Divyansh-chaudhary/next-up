a81c083c8eac6236f84571c85eb78a64790ba162
Divyansh-chaudhary <divyanshch2@gmail.com>
2020-07-17 13:47:35 -0800

    carousel 1 completed

--- js/main.js
+++ js/main.js
@@ -11,4 +11,44 @@ $(document).ready( function(){
         $("nav ul").toggleClass("ul-show");
         $("nav .social-icons").toggleClass("show-icons");
     });
+
+    /******* slick ************ */
+    if(screen.width < 470 ) {
+      $('.carousal-one').slick({
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        speed: 900,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+    }
 });
