75ad91cb78f2b9fd95fd547581020523f98468da
Divyansh-chaudhary <divyanshch2@gmail.com>
2020-07-19 17:13:12 -0800

    next up website is ready to published

--- js/main.js
+++ js/main.js
@@ -17,6 +17,8 @@ $(document).ready( function(){
       $('.carousal-one').slick({
         autoplay: true,
         dots: true,
+        prevArrow: false,
+        nextArrow: false,
         infinite: true,
         speed: 900,
         slidesToShow: 4,
@@ -50,5 +52,51 @@ $(document).ready( function(){
           // instead of a settings object
         ]
       });
+      $('.t-shirts').slick({
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        prevArrow: false,
+        nextArrow: false,
+        speed: 900,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
     }
+    $('.video-carousal').slick({
+      autoplay: true,
+      dots: false,
+      infinite: true,
+      speed: 900,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    });
 });
